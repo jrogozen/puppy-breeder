1dda78b70becfee5d41b65c5a3a5ada556464e7b
Jon Rogozen <jon.rogozen@gmail.com>
2014-08-29 10:37:08 -0700

    initialize puppy

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,5 +1,15 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
+	let(:pup) {PuppyBreeder::Puppy.new("Spot", "Pitbull", "Brown")}
 
+	describe '#initialize' do 
+		it "initializes instance of puppy class" do
+			expect(pup).to be_kind_of(PuppyBreeder::Puppy)
+		end
+
+		it "initializes correct color" do
+			expect(pup.color).to eq("Brown")
+		end
+	end
 end
