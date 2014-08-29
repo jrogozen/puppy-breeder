4a67e2afdd652fcba19f8fcbb556c21e0ca17161
Jon Rogozen <jon.rogozen@gmail.com>
2014-08-29 11:41:01 -0700

    begin purchase request class

--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,20 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
+	let(:jon) {PuppyBreeder::Breeder.new("Jon")}
+	let(:spot) {PuppyBreeder::Puppy.new("Spot", "Pitbull", "Brown")}
+	let(:puppylist) {PuppyBreeder::PuppyList.new}
+	let(:purchase_request) {PuppyBreeder::PurchaseRequest.new(spot, {:request_type => "verbal"})}
+
+	describe '#initialize' do
+		it "instantiates the purchase request class" do
+			expect(purchase_request).to be_kind_of(PuppyBreeder::PurchaseRequest)
+		end
+
+		it "properly sets optional variables" do
+			expect(purchase_request.request_type).to eq("verbal")
+			expect(purchase_request.payment_method).to be_nil
+		end
+	end
 
 end
